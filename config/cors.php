08cd1b9440ffe4062bd9d1ab3325d05b3cb4efa8
Simplyclc <simplyclc@users.noreply.github.com>
2024-10-09 21:11:34 -0400

    cors update

--- config/cors.php
+++ config/cors.php
@@ -25,9 +25,8 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
-    // 'exposed_headers' => ['*'],
-    
+    'exposed_headers' => ['*'],
+
     'max_age' => 0,
 
     'supports_credentials' => true,
