0e5ed16cd76964e685fb787c8c9c566e84e5d18c
Simplyclc <simplyclc@users.noreply.github.com>
2024-11-02 18:24:18 -0400

    updater kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () { DB::table('telescope_entries_tags')->delete(); })->hourly();
         $schedule->call(function () { DB::table('machines')->delete(); })->everyThreeHours($minutes = 0);
         $schedule->call(function () { DB::table('temps')->delete(); })->everyThreeHours($minutes = 0);
-        $schedule->call(function (){DB::table('personal_access_tokens')->delete();})->daily();
+        $schedule->call(function () {DB::table('personal_access_tokens')->delete();})->daily();
+        $schedule->call(function () {DB::table('telescope_entries')->delete();})->daily();
     }
 
     /**
